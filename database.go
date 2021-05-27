d8448b7f5dafe45bb884b47b40724c0e964ede6a
Mizeck Victor Tembo <temwanijr@gmail.com>
2021-05-27 14:26:00 +0200

    created structs

--- database.go
+++ database.go
@@ -1,8 +1,83 @@
 package main
 
+import (
+	"time"
+
+	_"github.com/gofiber/fiber/v2/middleware/timeout"
+	
+	"github.com/jinzhu/gorm"
+)
+
 /*Database tables and migrations
  */
 
 // Users ...
 type Users struct {
+	gorm.Model
+	UserID string 		`gorm:"type:varchar(20);not null" json:"userID"`
+	Firstname string 	`gorm:"type:varchar(20);not null" json:"firstname"`
+	Lastname string 	`gorm:"type:varchar(20);not null" json:"lastname"`
+	PhoneNumber string  `gorm:"type:varchar(20);not null unique" json:"phone_number"`
+	Password []byte		`gorm:"type:varbinary(50);not null" json:"password"`
+	Pin 	 []byte 	`gorm:"type:varbinary(20);not null unique" json:"pin"`
+	Username string 	`gorm:"type:varchar(255);not null unique" json:"username"`
+	UserTypeID int 		`gorm:"type:int;not null unique" json:"user_type_id"`
+	StatusID int 		`gorm:"type:int;not null unique" json:"status_id"`
+	Colour string 		`gorm:"type:varchar(255);not null unique" json:"color"`
+	Login_timeout time.Time `gorm:"type:time;not null unique" json:"login_timeout"`
+	Virtual_account_number string `gorm:"type:varchar(255);not null unique" json:"virtual_account_number"`
+	Avatar string 				`gorm:"type:varchar(255) not null" json:"avatar"`
+	Notification_token string 	`gorm:"type:varchar(50) not null unique" json:"notification"`
+	Email string 				`gorm:"type:varchar(50) not null unique" json:"email"`
 }
+
+type Account struct {
+	gorm.Model
+	UserID string `gorm:"type:varchar(20);not null" json:"userID"`
+	Balance int 	`gorm:"type:int;not null unique" json:"amount"`
+	AccountTypeID int `gorm:"type:int;not null unique" json:"account_type_id"`
+	StatusID int `gorm:"type:int;not null unique" json:"status_id"`
+}
+
+type UserType struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(255);not null unique" json:"user_type_name"`
+}
+type AccountType struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(255);not null unique" json:"acount_type_name"`
+}
+type Status struct{
+	gorm.Model
+	Name string	`gorm:"type:varchar(255);not null unique" json:"status_name"`
+}
+type Evoucher struct{
+	gorm.Model
+	EvoucherID string	`gorm:"type:varchar(20);not null unique" json:"evoucher_number"`
+	SenderNumber string	 `gorm:"type:varchar(20);not null unique" json:"sender's_number"`
+	ReceiverNumber string `gorm:"type:varchar(20);not null unique" json:"receiver's_number"`
+	Amount int 			`gorm:"type:int;not null unique" json:"amount"`
+}
+
+type Transaction struct{
+	gorm.Model
+	UserID string `gorm:"type:varchar(20);not null" json:"userID"`
+	BalanceBefore  int `gorm:"type:int;not null unique" json:"balance_before"`
+	Amount int `gorm:"type:int;not null unique" json:"amount"`
+	BalanceAfter int `gorm:"type:int;not null unique" json:"balance_after"`
+	StatusID int `gorm:"type:int;not null unique" json:"status_id"`
+	TransactionTypeID int `gorm:"type:int;not null unique" json:"transaction_type_id"`
+	TransferTypeID int `gorm:"type:int;not null unique" json:"transfer_type_id"`
+}
+
+type transactionType struct{
+	gorm.Model
+	Name string `gorm:"type:varchar(255);not null unique" json:"transaction_type_name"`
+}
+type TransferType struct{
+	gorm.Model
+	Name string `gorm:"type:varchar(255);not null unique" json:"transfer_type_name"`
+	Description string `gorm:"type:varchar(255);not null unique" json:"transfer_type_description"`
+}
+
+
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/go-kit/kit v0.10.0
+	github.com/gofiber/fiber/v2 v2.10.0
 	github.com/google/uuid v1.0.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.3.0
